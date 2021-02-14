ece7bec5a107a85adafc938f2b05a9499569b5cc
gujianteng <541051981@qq.com>
2021-02-15 05:28:22 +0800

    完成统一错误处理
    统一处理错误，包括async，swait的错误

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,23 +11,9 @@ var PostModel = require("../models/postModel")
 // 查询帖子，获取帖子列表
 exports.index = async (req, res) => {
 
-    try {
-        var date = await PostModel.find()
-        // 成功
-        res.send({
-            code: 0,
-            msg: "成功",
-            date: date
-        })
-    } catch (error) {
-        // 失败
-        console.log(error);
-        res.send({
-            code: -1,
-            msg: "失败"
-        })
-    }
-    // res.send("获取帖子列表")
+var date = await PostModel.find()
+// 成功
+res.send({code: 0, msg: "成功", date: date})
 }
 
 
@@ -35,79 +21,25 @@ exports.index = async (req, res) => {
 exports.create = async (req, res) => {
     //  获取前端传递过来的参数
     var { title, content } = req.body
-
-    // // Model.create() 写入数据库的方法
-    // PostModel.create({
-    //     title,
-    //     content
-    // })
-    // .then(()=>{
-    //     // 成功
-    //     res.send({
-    //         code:0,
-    //         msg:"成功"
-    //     })
-    // })
-    // .catch((error)=>{
-    // // 失败
-    // console.log(error);
-    // res.send({
-    //     code:-1,
-    //     msg:"失败"
-    // })
-    // })
-
-
     //  使用 async await  创建帖子
-    try {
-        await PostModel.create({
-            title,
-            content
-        })
-        // 成功
-        res.send({
-            code: 0,
-            msg: "成功"
-        })
+    await PostModel.create({title,content})
+    // 成功
+    res.send({code: 0,msg: "成功"})
 
-    } catch (error) {
-        // 失败
-        console.log(error);
-        res.send({
-            code: -1,
-            msg: "失败"
-        })
-    }
 }
 
 
 // 更新帖子
 exports.update = async(req, res) => {
     // 要更新的帖子的id,前端传过来动态的id
-    var id=req.params.id
-
+    var { id } = req.params
     // 更新的内容 req.body
     // var { title,content }= req.body
     // 使用数计库的 Model.updateOne方法操作
-    try {
-        // await PostModel.updateOne({ _id: id }, {title:title,content:content})
-        await PostModel.updateOne({ _id: id }, req.body)
-
-        // 成功
-        res.send({
-            code: 0,
-            msg: "成功",
-        })
-    } catch (error) {
-        // 失败
-        console.log(error);
-        res.send({
-            code: -1,
-            msg: "失败"
-        })
-    }
-    var { id } = req.params
-    // res.send("更新帖子")
+    // await PostModel.updateOne({ _id: id }, {title:title,content:content})
+    await PostModel.updateOne({ _id: id }, req.body)
+    // 成功
+    res.send({code: 0,msg: "成功"})
 }
 
 
@@ -117,20 +49,7 @@ exports.remove = async(req, res) => {
     // 获取id
     var {id}= req.params
     // 使用数计库的 Model.deleteOne方法操作
-
-    try {
-        await PostModel.deleteOne({_id:id})
-                // 成功
-                res.send({
-                    code: 0,
-                    msg: "成功",
-                })
-    } catch (error) {
-                // 失败
-                console.log(error);
-                res.send({
-                    code: -1,
-                    msg: "失败"
-                })
-    }
+    await PostModel.deleteOne({_id:id})
+            // 成功
+    res.send({code: 0,msg: "成功",})
 }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express=require('express')
+// 引入统一处理错误的包,不需要拿变量来接收
+require('express-async-errors')
+
 // 引入抽离出去的路由文件
 var  postRouter =require('./routers/postRouter')
 var app=express()
-
-
 //  req.body 中间件处理
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
@@ -14,6 +15,14 @@ app.use(express.urlencoded({ extended:true }))
 app.use(express.static('./public'))
 // 使用use方法调用路由文件，并设置好前缀
 app.use('/posts',postRouter)
+
+
+
+// 统一处理错误信息
+app.use((err, req, res, next) =>{
+    console.error(err)
+    res.status(500).send(err.message)
+  })
 
 
 app.listen(3000,function(){
